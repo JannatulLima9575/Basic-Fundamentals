49bc920287430aaa4b32eb835b9c3a7ac16945ac
Jannatul <limajannat1675@gmail.com>
2025-10-04 19:09:53 +0600

    added more three problem solving

--- SemesterOne/Assignment1/problem3.c
+++ SemesterOne/Assignment1/problem3.c
@@ -2,6 +2,14 @@
 
 int main() {
 
-    
+    int n;
+
+    scanf("%d", &n);
+
+    if (n % 3 == 0) {
+        printf("YES");
+    } else {
+        printf("NO");
+    }
     return 0;
 }
--- SemesterOne/Assignment1/problem5.c
+++ SemesterOne/Assignment1/problem5.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
 int main() {
+    long long N;
+    scanf("%lld", &N);
 
-    printf("hi");
+    if (N > 1000) {
+        printf("I will buy Punjabi\n");
+        N -= 1000;
+
+        if (N >= 500) {
+            printf("I will buy new shoes\n");
+            printf("Alisa will buy new shoes\n");
+        }
+    } else {
+        printf("Bad luck!\n");
+    }
+
     return 0;
 }
